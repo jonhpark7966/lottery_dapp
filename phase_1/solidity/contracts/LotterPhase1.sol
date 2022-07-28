92cdf43bae4b0b1970c44b7e2b74b6438da58db7
jonhpark7966 <35061738+jonhpark7966@users.noreply.github.com>
2022-07-28 19:12:55 +0900

    Update LotterPhase1.sol

--- phase_1/solidity/contracts/LotterPhase1.sol
+++ phase_1/solidity/contracts/LotterPhase1.sol
@@ -1,25 +1,45 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.7;
 
 // Import this file to use console.log
 import "hardhat/console.sol";
 
-contract LotteryPhase1 {
-    uint256 public drawTime;
-    address public owner;
-    mapping(address=>uint256) public amount;
+uint constant numberOfNumbers = 5;
 
+contract LotteryPhase1 {
+
+    struct NumberOfTicketsInWallet {
+      address owner;
+      uint256 numberOfTickets;
+    }
+    
+
+    uint256 public drawTime;
+    uint256 public N; // number range.
+    address public owner;
+    uint256 public ticketPrice;
+    // ticket map in public is not fair for former buyer
+    mapping(uint256=>NumberOfTicketsInWallet[]) public ticketMap;
     uint256 public totalAmount;
 
-    constructor(uint256 _drawTime){
+    // N is max num that users can choose.
+    constructor(uint256 _drawTime, uint8 _N, uint256 _ticketPrice){
         drawTime = _drawTime;
+        N = _N;
+        ticketPrice = _ticketPrice;
         owner = msg.sender;
     }
 
-    function buyticket() external payable{
+    function buyticket(uint256 ticketNumber, uint256 numberOfTicketToBuy) external payable{
         require(block.timestamp < drawTime, "This Lottery is Over");
+        require(msg.value == ticketNumber * ticketPrice, "Price does not match");
 
-        amount[msg.sender] += msg.value;
+        // TODO, handle replicated sender.
+        NumberOfTicketsInWallet memory currentRequest;
+        currentRequest.owner = msg.sender;
+        currentRequest.numberOfTickets = numberOfTicketToBuy;
+
+        ticketMap[ticketNumber].push(currentRequest);
         totalAmount += msg.value;
     }
 
@@ -27,12 +47,39 @@ contract LotteryPhase1 {
         // TODO, every body should be able to call draw, not only owner.
         // for now, only owner could call for test. 
         require(msg.sender == owner, "Temporal requirement, You are not owner!");
-
         require(block.timestamp > drawTime, "Not Finished Yet.");
 
-        // TODO, GET RANDOM to find out winner.
+        uint256 winner = rand();
 
-        // Pay it to winner.
-        payable(owner).transfer(totalAmount);
+        NumberOfTicketsInWallet[] memory winnerBuyers = ticketMap[winner];
+
+        if(winnerBuyers.length == 0){
+            // No winner this time.
+            return;
+        }
+
+        uint numWinnerTickets = 0;
+        for(uint i = 0; i < winnerBuyers.length; ++i){
+            numWinnerTickets += winnerBuyers[i].numberOfTickets;
+        }
+        for(uint i = 0; i < winnerBuyers.length; ++i){
+           address payable winnerAddress = payable(winnerBuyers[i].owner); 
+           winnerAddress.transfer(totalAmount/winnerBuyers[i].numberOfTickets);
+        }
+
     }
+
+// TODO, change pseudo random to oracle random.
+    function rand() public view returns(uint256)
+{
+    uint256 seed = uint256(keccak256(abi.encodePacked(
+        block.timestamp + block.difficulty +
+        ((uint256(keccak256(abi.encodePacked(block.coinbase)))) / (block.timestamp)) +
+        block.gaslimit + 
+        ((uint256(keccak256(abi.encodePacked(msg.sender)))) / (block.timestamp)) +
+        block.number
+    )));
+
+    return (seed - ((seed / (N**numberOfNumbers)) * (N**numberOfNumbers)));
+}
 }
